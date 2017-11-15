c25cbec8d4f46bcf93dbd8f7462cc46a770490ff
syzer3@gmail.com <syzer3@gmail.com>
2017-11-15 17:40:16 +0100

    missing lib folder

---
+++ back/src/lib/ml.js
@@ -0,0 +1,139 @@
+const natural = require('natural')
+let classifier = new natural.BayesClassifier()
+let companiesClassifier = new natural.BayesClassifier()
+const axios = require('axios')
+
+const companies = [
+  {label: 'AAPL', keywords: ['Apple']},
+  {label: 'CSG-U.TI', keywords: ['fan switch', 'Credit Suisse']},
+  {label: 'GOOG', keywords: ['Google', 'goog']},
+  {label: 'MSFT', keywords: ['Microsoft Corporation', 'Microsoft']},
+  {label: 'RBI.VI', keywords: ['BVI', 'Rising', 'ryzen', 'Raiffeisen']},
+  {label: 'SCMN.VX', keywords: ['Swisscom AG', 'swisscom']},
+  {label: 'ABB', keywords: ['ABB']},
+]
+// classifier = yield natural.BayesClassifier.load('./api/classifier.json', null)
+
+const intents = [
+  {
+    label: 'pay',
+    keywords: ['pay', 'find', 'hey', 'but', 'why', 'bite mark of year', 'find me a two beers I need two beers', 'find me a to pierce',
+      'pay  hey hey hey I want to pay five bucks to Martin and Mel',
+      'play 500 France tool metal',
+      'a package from school now',
+      'face shifting',
+      'hi jeff',
+      'mount a deer', // i guess its Mel's
+      'hatred for Gears', // mother flower
+    ]
+  },
+  {label: 'loan', keywords: ['loan']},
+  {label: 'invest', keywords: ['invest']},
+  {
+    label: 'summary',
+    keywords: ['summary', 'show me what my name is', 'month qqq', 'status', 'where my money went last month']
+  },
+  // those are hardcoded for the demo
+  {
+    label: 'sentence1',
+    keywords: [
+      'hey Jeff the Killer',
+      'stage of beer',
+      'play Jeff beer',
+      'qqqq Jeff I beer',
+      'Justin Bieber',
+      'Asia beer',
+      'a German qqq',
+      'Jeff',
+      'appear'
+    ]
+  },
+  {
+    label: 'sentence2', keywords: [
+    'how much money do I have left',
+    'how much money do I have',  // over fit him
+    'how much money do I have'
+  ]
+  },
+  {
+    label: 'sentence3', keywords: [
+    'I need to borrow money',
+    'I have to borrow money',
+    'I need to borrow a mommy',
+    'Sbarro online',
+    'I want to borrow a money',
+    'I want that blow money'
+  ]
+  },
+  {
+    label: 'sentence4', keywords: [
+    'veterans hundred francs to Paul for the bike',
+    'return hundred francs for France for the bike',
+    'return hundred Francs',
+    'Britain and France to fall from the bike',
+    'returning from assistance'
+  ]
+  },
+  {label: 'sentence5', keywords: ['I want to invest my money', 'I want to invest money']},
+  {
+    label: 'sentence6', keywords: ['Where are my friends investing',
+    'where are my Preston besti', // mother of god
+    'where are my friends investment']
+  },
+  {
+    label: 'sentence7', keywords: ['Am I poor?', 'am I for', 'mi4']
+  },
+  {
+    label: 'sentence8', keywords: ['I want to buy some Apple stocks', 'I want to buy qqqq of Apple stocks',
+    'I want to buy qqqq stocks',
+    'qqqq about fun',
+    'what about sometimes',
+    'what about publications'
+  ]
+  },
+  {
+    label: 'askStockPrice',
+    keywords: ['what\'s the master price', 'what\'s on my', 'monster Google stock price', 'what\'s stock price']
+  },
+  {
+    label: 'cannotUnderstand', keywords: [
+    'I don\'t understand', 'lalalalala',
+    'blah blah blah blah blah', 'Milano', 'I\'m tired', 'Idora Park',
+    'I do not want to talk',
+    'qqqq lalalala qqqq',
+    'lala'
+  ]
+  },
+  {
+    label: 'say', keywords: ['say qqq', 'save qqq', 'seiwa qqq', 'same qqq', 'safe qqq']
+  }
+]
+
+intents
+  .map(intent =>
+    intent.keywords.map(text => classifier.addDocument(text, intent.label)))
+
+classifier.train()
+classifier.save('./data/classifier.json')
+
+companies.map(company =>
+  company.keywords.map(text => companiesClassifier.addDocument(text, company.label)))
+
+companiesClassifier.train()
+companiesClassifier.save('./data/classifier.companies.json')
+
+module.exports = {
+  // classify usr intent
+  classify: (sentence) => {
+    const result = classifier.getClassifications(sentence).shift()
+    return {
+      label: result.label,
+      confidence: result.value,
+    }
+  },
+  // gets remote intent
+  getIntent: (sentence) =>
+    axios.get(`http://robot-machine.herokuapp.com/intent?statement=${sentence}`)
+      .then(({data}) => data),
+  classifyCompanySymbol: (sentence) => companiesClassifier.getClassifications(sentence).shift().label
+}
